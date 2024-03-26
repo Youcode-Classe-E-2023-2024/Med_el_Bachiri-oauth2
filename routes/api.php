1634fc4b90961e0da1d1a0d28da33cd301565f57
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-26 09:53:16 +0000

    setting api auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\isAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+    });
+
+    Route::middleware('auth:api')->group( function () {
+        Route::get('user', [UserController::class, 'getUserDetails'])->middleware(isAdmin::class);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
