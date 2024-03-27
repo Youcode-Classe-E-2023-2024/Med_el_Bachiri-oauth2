bd1b026de6020544e754b8a2df2868db3ed8153d
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-27 13:13:04 +0000

    User model with crud role methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,47 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_has_role');
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class);
+    }
+
+    public function hasRole($role)
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+
+    public function assignRole($role)
+    {
+        if (is_string($role)) {
+            $role = Role::whereName($role)->firstOrFail();
+        }
+        $this->roles()->syncWithoutDetaching($role);
+    }
+
+    public function removeRole($role)
+    {
+        if (is_string($role)) {
+            $role = Role::whereName($role)->firstOrFail();
+        }
+        $this->roles()->detach($role);
+    }
+    public function changeRole($newRole)
+    {
+        $role = Role::where('name', $newRole)->first();
+        if (!$role) {
+            return false;
+        }
+
+        $this->roles()->detach();
+        $this->assignRole($newRole);
+        return true;
+    }
+
 }
