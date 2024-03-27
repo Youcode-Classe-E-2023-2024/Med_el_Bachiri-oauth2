fddde2d2ac354a63e0a0b71bed6c6beb2a43486f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-03-27 15:37:38 +0000

    change pw routes

--- routes/api.php
+++ routes/api.php
@@ -28,14 +28,16 @@ Route::namespace('Api')->group(function () {
 
         Route::get('me', [UserController::class, 'getUserDetails']);
 
-        Route::middleware('permission:view-users')->group(function () {
+        Route::middleware('permission:manage-users')->group(function () {
             Route::get('users', [UserController::class, 'getAllUsers']);
             Route::prefix('user')->group(function () {
                 Route::post('create', [AuthController::class, 'register']);
                 Route::delete('delete/{user_id}', [UserController::class, 'delete']);
                 Route::put('update/{user_id}', [UserController::class, 'update']);
+                Route::put('password/new/{user_id}',[UserController::class, 'updateUserPw']);
             });
         });
+        Route::put('/me/password/new', [UserController::class, 'updateMyPw']);
         Route::middleware('permission:manage-roles')->group(function () {
 
         });
